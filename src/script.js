fdbb802ca3d1be140c9bfbc2abe057812cd76731
popoxta <popoxta@gmail.com>
2023-03-24 19:20:09 +1300

    add metric/imperial switch and styles

--- src/script.js
+++ src/script.js
@@ -1,47 +1,59 @@
 import {getFormattedWeather} from "./weatherdata.js";
 
+let isMetric = false
+
 function displayWeather(data){
-    const header = document.querySelector('.header > h1')
-    header.textContent = data.city
+    const cityName = document.createElement('h1')
+    cityName.textContent = data.city
+    const country = document.createElement('h2')
+    country.textContent = data.country
+    const header = document.querySelector('.header')
+    header.replaceChildren(cityName, country)
 
-    const subHeader = document.querySelector('.header > h2')
-    subHeader.textContent = data.country
-
-    const temp = document.querySelector('.temp > h3')
-    temp.textContent = data.tempC // todo c/f temp based
-
-    const tempDetails = document.querySelector('.temp-details')
     const condition = document.createElement('p')
     condition.textContent = data.condition
-    const feelsLike = document.createElement('p')
-    feelsLike.textContent = `Feels like ${data.feelsC}` // todo c/f temp based
     const humidity = document.createElement('p')
     humidity.textContent = `Humidity ${data.humidity}`
-
+    const feelsLike = document.createElement('p')
+    const tempDetails = document.querySelector('.temp-details')
     tempDetails.replaceChildren(condition, feelsLike, humidity)
-}
 
-const citySearch = document.querySelector('form')
-citySearch.addEventListener('submit', e => {
-    e.preventDefault()
-    const query = document.querySelector('#query').value
-    getFormattedWeather(query)
-        .then(weather => {
-            displayWeather(weather)
-            queryFeedback(false)
-        })
-        .catch(err => {
-            console.log(err)
-            queryFeedback(true)
-        })
-})
+    const temp = document.createElement('h3')
+    const tempDisplay = document.querySelector('.temp')
+    tempDisplay.replaceChildren(temp)
 
-function queryFeedback(bool){
-    if (bool){
-        citySearch.className = 'bad-query'
-    } else {
-        citySearch.className = ''
+    temp.addEventListener('click', e => {
+        isMetric = !isMetric
+        setTemperatureUnits()
+    })
+
+    setTemperatureUnits()
+    function setTemperatureUnits(){
+        temp.className = isMetric ? 'unit-c' : 'unit-f'
+        temp.textContent = isMetric ? data.tempC : data.tempF
+        feelsLike.textContent = `Feels like ${isMetric ? data.feelsC : data.feelsF}`
     }
 }
 
-const akl = getFormattedWeather('auckland').then(weather => displayWeather(weather)).catch(err => console.log(err))
+const citySearch = document.querySelector('form')
+citySearch.addEventListener('submit', async e => {
+    e.preventDefault()
+    const query = document.querySelector('#query').value
+
+    try {
+        const weather = await getFormattedWeather(query)
+        displayWeather(weather)
+        showBadQuery(false)
+    } catch (err){
+        console.log(err)
+        showBadQuery(true)
+    }
+})
+
+function showBadQuery(bool){
+    citySearch.className = bool ? 'bad-query' : ''
+}
+
+getFormattedWeather('auckland')
+    .then(displayWeather)
+    .catch(console.log)
--- src/weatherdata.js
+++ src/weatherdata.js
@@ -3,30 +3,28 @@ export {getFormattedWeather}
 const API_KEY = '0d034dfff4894e92825194255232203'
 const ENDPOINT = 'https://api.weatherapi.com/v1/current.json'
 
-async function getWeatherData(query){
-    return await fetch(`${ENDPOINT}?key=${API_KEY}&q=${query}`,
+async function getWeatherData(query) {
+    const res = await fetch(`${ENDPOINT}?key=${API_KEY}&q=${query}`,
         {
-            mode : 'cors'
+            mode: 'cors'
         })
-        .then(res => {
-            if (res.status === 400) throw new Error('Query not found')
-            else return res.json()
-        })
+    if (res.status === 400) throw new Error('Query not found')
+    else return res.json()
 }
 
-function formatWeatherData(data){
+function formatWeatherData(data) {
     return {
-        country : data.location.country,
-        city : data.location.name,
-        condition : data.current.condition.text,
-        tempC : data.current.temp_c,
-        feelsC : data.current.feelslike_c,
-        tempF : data.current.temp_f,
-        feelsF : data.current.feelslike_f,
-        humidity : data.current.humidity
+        country: data.location.country,
+        city: data.location.name,
+        condition: data.current.condition.text,
+        tempC: Math.floor(data.current.temp_c),
+        feelsC: Math.floor(data.current.feelslike_c),
+        tempF: Math.floor(data.current.temp_f),
+        feelsF: Math.floor(data.current.feelslike_f),
+        humidity: data.current.humidity
     }
 }
 
-function getFormattedWeather(query){
-    return getWeatherData(query).then(data => formatWeatherData(data))
+function getFormattedWeather(query) {
+    return getWeatherData(query).then(formatWeatherData)
 }
